4f43c3f11b23da4cdd8ba4306be10ac4f395c5ec
Alpheus <69361507+alpheus23@users.noreply.github.com>
2020-11-14 22:49:21 -0500

    created relation to database

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.Measurement
+Station = Base.classes.station
 
 app = Flask(__name__)
 
