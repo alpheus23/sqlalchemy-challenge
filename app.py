8025453abce219a1e213e39f06812a46eb5011f2
Alpheus <69361507+alpheus23@users.noreply.github.com>
2020-11-14 23:00:55 -0500

    Completed first route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import datetime as dt
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -11,7 +12,7 @@ engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-Measurement = Base.classes.Measurement
+Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 app = Flask(__name__)
@@ -26,6 +27,25 @@ def home():
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start>"
     )
+
+@app.route('/api/v1.0/precipitation')
+def prcp():
+    session = Session(engine)
+
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    dates = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= prev_year).all()
+
+    session.close()
+
+    all_prcp = []
+    for date, prcp in dates:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        all_prcp.append(prcp_dict)
+
+    return jsonify(all_prcp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
